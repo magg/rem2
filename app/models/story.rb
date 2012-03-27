70e373925c2a08b610cf37a5a1ca90541fb0c525
Miguel Gonzalez <maggonzz@gmail.com>
2012-03-26 19:12:59 -0600

    model validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,8 @@
 class Story < ActiveRecord::Base
 # validation stuff...
  has_one :status
+ has_many :sprintstories
+ has_many :sprints :through => :sprintstories
  validates :name, :descripcion, :prioridad, :usuario, :presence => true
  validates :functionpts, :numericality => true
  validates :name, :uniqueness => true, :length => { 
