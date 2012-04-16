d1c3d143d6a4e1cb97124ff7fc849540ac1b8be7
Manuel Castaneda <castaneda.m@gmail.com>
2012-04-16 15:44:56 -0500

    validaciones usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,21 +1,12 @@
 class Usuario < ActiveRecord::Base
-<<<<<<< HEAD
-	has_many :admins, :clients, :students
-	validates :username, :password_digest, :tipo, :presence => true
-=======
-	has_one :admin
-	has_one :client
-	has_one :student
-	
-	validates :username, :presence => true, :uniqueness => true
-
+  has_one :admin
+  has_one :client
+  has_one :student
+  validates :username, :presence => true, :uniqueness => true
   has_secure_password
   attr_accessible :username, :password, :password_confirmation, :tipo
   validates_presence_of :password, :on => :create
-
-  def self.authenticate(email, password)
-    find_by_email(email).try(:authenticate, password)
+  def self.authenticate(username, password)
+    find_by_username(username).try(:authenticate, password)
   end
-	
->>>>>>> students
 end
