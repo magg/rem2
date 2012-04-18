c8a3bdae503a9be3cb535245751b8148b138caa8
Miguel Gonzalez <maggonzz@gmail.com>
2012-04-17 22:57:35 -0500

    modelos de team/ project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
 	belongs_to :client
+	has_one :team
 	validates :nombre, :descripcion, :periodo, :presence => true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
 	has_many :students
+	belongs_to :project
  	validates :nombre, :presence => true
 end
