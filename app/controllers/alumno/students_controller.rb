85e3da29f100813a2ba69e0c70d0243d1d3765e3
Miguel Gonzalez <maggonzz@gmail.com>
2012-04-19 00:09:01 -0500

    problemillas con students y clientes resolved!!

--- app/controllers/alumno/students_controller.rb
+++ app/controllers/alumno/students_controller.rb
@@ -4,6 +4,9 @@ class Alumno::StudentsController < ApplicationController
   # GET /students.json
   def index
     @students = Student.all
+    usuario = Usuario.find_by_auth_token( cookies[:auth_token])
+    #student = Student.where(:usuario_id => usuario.id)
+    @id = usuario.id
 
     respond_to do |format|
       format.html # index.html.erb
@@ -14,26 +17,36 @@ class Alumno::StudentsController < ApplicationController
 
   # GET /students/1/edit
   def edit
-     @student = Student.find(params[:id])
-      @usuario = @student.usuario  
+     student = Student.find(params[:id])
+     usuario = Usuario.find_by_auth_token( cookies[:auth_token])
+     if student.usuario_id == usuario.id
+       @student = student
+       @usuario = @student.usuario
+     else 
+       redirect_to alumno_students_path, :notice => "Usted no tiene permisos suficientes"
+       
+     end
   end
 
   # PUT /students/1
   # PUT /students/1.json
   def update
     @student = Student.find(params[:id])
-
-    respond_to do |format|
-      if @student.update_attributes(params[:student]) and 
-        @student.usuario.update_attributes(params[:student][:usuario_attributes])
-        format.html { redirect_to [:alumno, @student], notice: 'Student was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @student.errors, status: :unprocessable_entity }
+    usuario = Usuario.find_by_auth_token( cookies[:auth_token])
+    if @student.usuario_id == usuario.id
+      respond_to do |format|
+        if @student.update_attributes(params[:student]) and 
+          @student.usuario.update_attributes(params[:student][:usuario_attributes])
+          format.html { redirect_to [:alumno, @student], notice: 'Student was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @student.errors, status: :unprocessable_entity }
+        end
       end
-    end
+   end
   end
+  
  protected
     def authorize_student
       #unless Usuario.find_by_id(session[:user_id])
