a8e3c90a6c5c23a726df99406c52330fe88477da
Manuel Castaneda <castaneda.m@gmail.com>
2012-04-24 21:17:24 -0500

    seeds jajajajaja

--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -1,20 +1,9 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  test "function points must be positive" do
-product = Story.new(:name => 'Log In Cliente', :status_id => '1', :descripcion => 'El sistema permite al cliente tener acceso haciendo useo de sus datos personales', :prioridad => 'Alta', :usuario => 'Panchito', :comentarios => 'Hacer uso de Cifrado')
-product.functionpts = -1
-assert product.invalid?
-#assert_equal "must be greater than or equal to 0.01",
-#product.errors[:functionpts].join('; ')
-
-product.functionpts = 0
-assert product.invalid?
-#assert_equal "must be greater than or equal to 0.01",
-#product.errors[:functionpts].join('; ')
-
-product.functionpts = 1
-assert product.valid?
-end
-
+	test "functionpts price must be positive" do
+	  product = Story.new(:name => 'Conexion con Bancos', :status_id =>     '5', :descripcion => 'Deben de existir conexiones con las DB de los bancos', :prioridad => 'Media', :usuario => 'Lorena', :comentarios => 'Checar seguriad de Conexion')
+	  product.functionpts = 1
+	  assert product.valid?, "Si es mayor"
+	end
 end
